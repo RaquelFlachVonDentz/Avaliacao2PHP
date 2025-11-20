409ceed4021a1f2b2b740be23f85a30c5ba18aa9
RaquelFlachVonDentz <raquelflach_1995@hotmail.com>
2025-11-19 22:14:02 -0300

    19.11.202522h13min

--- app/Controllers/SiteController.php
+++ app/Controllers/SiteController.php
@@ -3,22 +3,37 @@
 namespace App\Controllers;
 
 use App\Core\View;
+use App\Repositories\ProductRepository;
+use App\Repositories\CategoryRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class SiteController
 {
     private View $view;
+    private ProductRepository $productRepo;
+    private CategoryRepository $categoryRepo;
 
     public function __construct()
     {
         $this->view = new View();
+        $this->productRepo = new ProductRepository();
+        $this->categoryRepo = new CategoryRepository();
     }
 
     public function index(Request $request): Response
     {
-        $congrats = $request->query->get('congrats', false);
-        $html = $this->view->render('site/index', compact('congrats'));
+        $products = $this->productRepo->findAll();
+        $categories = $this->categoryRepo->getArray();
+        
+        // Limita a 6 produtos para a seção de promoções
+        $featuredProducts = array_slice($products, 0, 6);
+        
+        $html = $this->view->render('site/index', [
+            'products' => $products,
+            'featuredProducts' => $featuredProducts,
+            'categories' => $categories
+        ]);
         return new Response($html);
     }
 }
